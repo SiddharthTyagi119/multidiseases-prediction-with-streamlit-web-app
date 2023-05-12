5c9505b75dc13be3f6614f730830bff5fa1f93df
siddharth Tyagi <sidtyagi05@gmail.com>
2023-05-12 06:00:56 +0530

    updated main.py

--- mdps_public.py
+++ mdps_public.py
@@ -5,11 +5,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('E:\Data science\Projects\multidiseases-prediction-with-streamlit-web-app\diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('E:\Data science\Projects\multidiseases-prediction-with-streamlit-web-app\heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('E:\Data science\Projects\multidiseases-prediction-with-streamlit-web-app\parkinsons_model.sav', 'rb'))
 
 
 
